710672f1d1b7ea7efd9b8565e66b0abb54f82318
pythonbuzzofficial <pythonbuzz.official@gmail.com>
2025-06-16 19:12:20 +0530

    Add files via upload

--- adjacencylist_insertoperations.py
+++ adjacencylist_insertoperations.py
@@ -15,33 +15,67 @@ def add_edge(v1,v2):
     graph[v2].append(v1)
     
 #for directed graph
-def add_edge(v1,v2):
-    if v1 not in graph or v2 not in graph:
-        print("vertex not found")
-        return
-    graph[v1].append(v2)
+# def add_edge(v1,v2):
+#     if v1 not in graph or v2 not in graph:
+#         print("vertex not found")
+#         return
+#     graph[v1].append(v2)
 
 #for undirected weighted graph   
-def add_edge(v1,v2,cost):
-    if v1 not in graph or v2 not in graph:
-        print("vertex not found")
-        return
-    graph[v1].append((v2,cost))
-    graph[v2].append((v1,cost))
+# def add_edge(v1,v2,cost):
+#     if v1 not in graph or v2 not in graph:
+#         print("vertex not found")
+#         return
+#     graph[v1].append((v2,cost))
+#     graph[v2].append((v1,cost))
 
 #for directed weighted graph 
-def add_edge(v1,v2,cost):
-    if v1 not in graph or v2 not in graph:
-        print("vertex not found")
-        return
-    graph[v1].append((v2,cost))
+# def add_edge(v1,v2,cost):
+#     if v1 not in graph or v2 not in graph:
+#         print("vertex not found")
+#         return
+#     graph[v1].append((v2,cost))
 
+def delete_vertex(vertex):
+    graph.pop(vertex)
+    for i in graph:
+        if vertex in graph[i]:
+            graph[i].remove(vertex)
+
+def delete_vertex_weighted(vertex):
+    graph.pop(vertex) 
+    for i in graph:
+        for j in graph[i]:
+            if vertex in j:
+                graph[i].remove(j)
+
+def delete_edge(v1,v2):
+    if v2 in graph[v1]:
+        graph[v1].remove(v2)
+    
+    if v1 in graph[v2]:
+        graph[v2].remove(v1)
+    
+def delete_weighted_edge(v1,v2):
+    for edge in graph[v1]:
+        if v2 in edge:
+            graph[v1].remove(edge)
+    for edge in graph[v2]:
+        if v1 in edge:
+            graph[v2].remove(edge)   
 
 add_vertex('A')
 add_vertex('B')
 add_vertex('C')
-print(graph)
+add_vertex('D')
 
 add_edge('A','B')
 add_edge('A','C')
+add_edge('C','D')
+add_edge('B','D')
+print(graph)
+
+# delete_vertex('D')
+# print(graph)
+delete_edge('C','D')
 print(graph)
--- adjacencymatirx_insertoperation.py
+++ adjacencymatirx_insertoperation.py
@@ -23,41 +23,67 @@ def add_edge(V1,V2):
     matrix[j][i] =1
 
 #for directed graph
-def add_edge(V1,V2):
-    if V1 not in vertices or V2 not in vertices:
-        print("Vertex not found")
-        return
-    i = vertices.index(V1)
-    j = vertices.index(V2)
-    matrix[i][j] =1
+# def add_edge(V1,V2):
+#     if V1 not in vertices or V2 not in vertices:
+#         print("Vertex not found")
+#         return
+#     i = vertices.index(V1)
+#     j = vertices.index(V2)
+#     matrix[i][j] =1
 
 #for undirected weighted graph   
-def add_edge(V1,V2,cost):
-    if V1 not in vertices or V2 not in vertices:
-        print("Vertex not found")
-        return
-    i = vertices.index(V1)
-    j = vertices.index(V2)
-    matrix[i][j] =cost
-    matrix[j][i] =cost
+# def add_edge(V1,V2,cost):
+#     if V1 not in vertices or V2 not in vertices:
+#         print("Vertex not found")
+#         return
+#     i = vertices.index(V1)
+#     j = vertices.index(V2)
+#     matrix[i][j] =cost
+#     matrix[j][i] =cost
 
 #for directed weighted graph   
-def add_edge(V1,V2,cost):
-    if V1 not in vertices or V2 not in vertices:
-        print("Vertex not found")
-        return
-    i = vertices.index(V1)
-    j = vertices.index(V2)
-    matrix[i][j] =cost
+# def add_edge(V1,V2,cost):
+#     if V1 not in vertices or V2 not in vertices:
+#         print("Vertex not found")
+#         return
+#     i = vertices.index(V1)
+#     j = vertices.index(V2)
+#     matrix[i][j] =cost
+
+def delete_vertex(vertex):
+    v_index = vertices.index(vertex)
+    matrix.pop(v_index)
+    for row in matrix:
+        row.pop(v_index)
+    vertices.remove(vertex)
+
+def delete_edge(v1,v2):
+    i = vertices.index(v1)
+    j =vertices.index(v2)
+    matrix[i][j] =0
+    matrix[j][i]=0
 
 
 add_vertex('A')
 add_vertex('B')
 add_vertex('C')
+add_vertex('D')
+
 print(vertices)
 
 add_edge('A','B')
 add_edge('B','C')
+add_edge('A','C')
+add_edge('A','D')
+add_edge('D','C')
 print(matrix)
+for row in matrix:
+    print(*row)
+print(" ")
+# delete_vertex('D')
+# for row in matrix:
+#     print(*row)
+# print(vertices)
+delete_edge('A','B')
 for row in matrix:
     print(*row)
